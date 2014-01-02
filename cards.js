2cdc94bbfe0b7d781934cd340b401f2dc297cab7
Leon Kunert <leonkunert@me.com>
2014-01-02 04:14:31 +0100

    Added Cards function and able to create 3 different deck types

--- cards.js
+++ cards.js
@@ -1,3 +1,4 @@
+var _ = require("underscore");
 // Cards functions
 var playingCards = function (options) {
     // Defaults
@@ -9,17 +10,94 @@ var playingCards = function (options) {
     // Extending defaults with options
     this.config = objExtend(this.defaults, options);
 
+    this.init();
+
+    return this;
+};
+
+var suits = {
+    'standart': {
+        "♠": "Spades",
+        "♦": "Diamonds",
+        "♣": "Clubs",
+        "♥": "Hearts"
+    }
+};
+
+var ranks = {
+    'twoToA': {
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        "10": "Ten",
+        "J": "Jack",
+        "Q": "Queen",
+        "K": "King",
+        "A": "Ace"
+    },
+    'sevenToA': {
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        "10": "Ten",
+        "J": "Jack",
+        "Q": "Queen",
+        "K": "King",
+        "A": "Ace"
+    },
+    'nineToA': {
+        "9": "Nine",
+        "10": "Ten",
+        "J": "Jack",
+        "Q": "Queen",
+        "K": "King",
+        "A": "Ace"
+    }
+};
+
+playingCards.prototype.init = function () {
     switch (this.config.deckType) {
         case "french":
             console.log('french');
+            this.config.suits = suits.standart;
+            this.config.ranks = ranks.twoToA;
             break;
         case "piquet":
             console.log('piquet');
+            this.config.suits = suits.standart;
+            this.config.ranks = ranks.sevenToA;
             break;
         case "doppelkopf":
             console.log('doppelkopf');
+            this.config.suits = suits.standart;
+            this.config.ranks = ranks.nineToA;
             break;
     }
+    this.cards = [];
+    var o = this.config,
+        l, i, s, r, j;
+    // populate draw pile
+    for (i = 0; i < o.decks; i++) {
+        // standard
+        for (s in o.suits) {
+            for (r in o.ranks) {
+                l = this.cards.length;
+                this.cards[l] = new card(r, o.ranks[r], s, o.suits[s]);
+            }
+        }
+    }
+}
+
+var card = function (rank, suit, value) {
+    this.rank       = rank;
+    this.suit       = suit;
+    this.value      = value;
+    return this;
 };
 
 // Helpers
--- test/test.js
+++ test/test.js
@@ -1,37 +1,65 @@
 // Inculuding Assert module and the Cardsjs
 var assert         = require("assert")
+    , _            = require("underscore")
     , playingCards = require("../cards.js");
 
-describe('Setup', function(){
-    it('should just be succeeding', function(){
+describe('Setup', function () {
+    it('should just be succeeding', function () {
         assert.equal(1, 1);
         assert.equal(-1, -1);
     })
 });
 
-describe('Real Tests', function(){
-    describe('Structural', function(){
-        describe('Defaults Should be', function(){
+describe('Real Tests', function () {
+    describe('Structural', function () {
+        describe('Defaults Should be', function () {
             var cards = new playingCards();
-            it('One for Decks', function(){
+            it('One for Decks', function () {
                 assert.equal(cards.defaults.decks, 1);
             });
-            it('Poker for Deck Type', function(){
+            it('4 suits', function () {
+                assert.equal(_.size(cards.config.suits), 4);
+            });
+            it('13 ranks', function () {
+                assert.equal(_.size(cards.config.ranks), 13);
+            });
+            it('Poker for Deck Type', function () {
                 assert.equal(cards.defaults.deckType, 'french');
+            });
+            it('with 52 cards', function () {
+                assert.equal(_.size(cards.cards), 52);
             });
         });
     });
     describe('Configurables', function () {
         describe('piquet', function () {
             var cards = new playingCards({"deckType":'piquet'});
-            it('Create a new deck of cards of the type piquet', function(){
+            it('Create a new deck of cards of the type piquet', function () {
                 assert.equal(cards.config.deckType, 'piquet');
             });
+            it('with 4 suits', function () {
+                assert.equal(_.size(cards.config.suits), 4);
+            });
+            it('with 8 ranks', function () {
+                assert.equal(_.size(cards.config.ranks), 8);
+            });
+            it('with 32 cards', function () {
+                assert.equal(_.size(cards.cards), 32);
+            });
         });
-        describe('doppelkopf', function(){
+        describe('doppelkopf', function () {
             var cards = new playingCards({"deckType":'doppelkopf'});
-            it('Create a new deck of cards of the type dopplekopf', function(){
+            it('Create a new deck of cards of the type dopplekopf', function () {
                 assert.equal(cards.config.deckType, 'doppelkopf');
+            });
+            it('with 4 suits', function () {
+                assert.equal(_.size(cards.config.suits), 4);
+            });
+            it('with 6 ranks', function () {
+                assert.equal(_.size(cards.config.ranks), 6);
+            });
+            it('with 24 cards', function () {
+                assert.equal(_.size(cards.cards), 24);
             });
         });
     });
